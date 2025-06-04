351eaf6143befd7044f3ffcc2ef6578d8091869b
AndreDG88 <andre.soares.designer@gmail.com>
2025-06-04 17:25:58 -0300

    Atualização em urls.py e .env

--- backend/trocaumaideia/urls.py
+++ backend/trocaumaideia/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,7 +27,13 @@ from rest_framework_simplejwt.views import (
 
 from deploy.views import github_webhook
 
+
+def home_view(request):
+    return JsonResponse({"mensagem": "API online."})
+
+
 urlpatterns = [
+    path("", home_view),
     path("admin/", admin.site.urls),
     path("api/", include("talkzone.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
