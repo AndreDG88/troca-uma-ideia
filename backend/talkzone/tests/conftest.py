5d9b8abf32e29db7c3819ac5bdcd511931e03eb0
AndreDG88 <andre.soares.designer@gmail.com>
2025-06-16 15:33:47 -0300

    correção dos fixtures

--- backend/talkzone/tests/conftest.py
+++ backend/talkzone/tests/conftest.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from talkzone.models import Profile
+from talkzone.models import Profile, Tweet
 
 
 @pytest.fixture
-def create_user_and_token(db):
+def create_user(db):
     user = User.objects.create_user(username="testuser", password="testpass123")
     profile = Profile.objects.get(user=user)
     profile.bio = "Bio original"
@@ -37,3 +37,9 @@ def auth_client(create_user_and_token):
     client = APIClient()
     client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
     return client
+
+
+@pytest.fixture
+def tweet(db, create_user):
+    tweet = Tweet.objects.create(user=create_user, content="Test tweet content")
+    return tweet
